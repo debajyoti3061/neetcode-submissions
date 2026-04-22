569c8f75d10ee5f3a589a93e324ca1cb490f8c36
debajyoti3061 <debajyoti.3061@gmail.com>
2026-04-22 15:39:44 -0700

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def helper(nums):
+            if not nums:
+                return 0
+            if len(nums) == 1:
+                return nums[0]
+            
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[1], dp[0])
+            
+            for i in range(2, len(nums)):
+                dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        
+            return dp[-1]
+        
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        
+        return max(helper(nums[:n-1]), helper(nums[1:]))
